a3faa558289371ca4d9d33be6cf2700d90ccaad4
Elisa Garcia <71991443+ElisaGarcia12@users.noreply.github.com>
2024-03-04 07:18:28 -0600

    git

--- WebScrapperCode.py
+++ WebScrapperCode.py
@@ -3,11 +3,12 @@ import requests
 
 url = "https://www.msn.com/en-us/weather/forecast/in-League-City,TX"
 page = requests.get(url)
+# used html.parser because it's the best for post 3.2 versions
 soup = BeautifulSoup(page.text, "html.parser")
-job = soup.find()
-daily_weather = job.find()
 
-print(daily_weather)
+league_city_daily = soup.find_all("li", class_="display:inline")
 
-
-
+for weather in league_city_daily:
+    info = {}
+    info['Day'] = weather.find("p", class_="headerV3-DS-EntryPoint1-1")
+    info['High'] = weather.find("topTemp-E1_1 temp")
